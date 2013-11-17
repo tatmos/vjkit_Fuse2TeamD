68f24d702df36cc3c876d246d49bc6c54a7a6aa6
tatmos <cha@bb.wakwak.com>
2013-11-18 05:26:55 +0900

    新規作成
    TeamDフォルダ以下に各自作業フォルダを作成して下さい。
    マージするシーンはTeamD/teamD.unityです。
    GitHubに上げるフォルダはAssets/とProjectSettings/以下のファイルのみでOK（/Libraryとかmetaは各自のロー
    カルに自動生成されます。）

---
+++ Assets/TeamD/tatmos/YukaMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YukaMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public float speed = 4f;
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(speed*Time.deltaTime,0,0);
+	}
+}
